fb199c8f1d6911fb892d1d50a1e28a1e4b53d200
vereschaka84 <vereschaka84@gmail.com>
2023-04-09 19:56:58 +0300

    new site

---
+++ script.js
@@ -0,0 +1,5 @@
+function soundClick() {
+	var audio = new Audio(); // Создаём новый элемент Audio
+	audio.src = 'img/Ou-eeee-sms.mp3'; // Указываем путь к звуку "клика"
+	audio.autoplay = true; // Автоматически запускаем
+}
