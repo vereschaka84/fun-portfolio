d6e85a744235a1cec5d3aa5a1a35863fe367509d
vereschaka84 <vereschaka84@gmail.com>
2023-04-10 11:25:44 +0300

    changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function soundClick() {
-	const audio = new Audio();
-	audio.src = 'img/Ou-eeee-sms.mp3';
-	audio.autoplay = true;
+	var audio = new Audio(); // Создаём новый элемент Audio
+	audio.src = 'img/Ou-eeee-sms.mp3'; // Указываем путь к звуку "клика"
+	audio.autoplay = true; // Автоматически запускаем
 }
