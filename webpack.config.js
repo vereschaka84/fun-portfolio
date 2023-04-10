68c9a9ff3e77fdd60a5837f2ddb8c56e113f80db
vereschaka84 <vereschaka84@gmail.com>
2023-04-10 22:10:42 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	mode: 'production',
 	entry: './src/script.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js',
 	},
 
